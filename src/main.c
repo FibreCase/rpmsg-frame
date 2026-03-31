120d10f90cfc8bd3e5c79f7adb9e94306c156663
fibre <fibre@FibreNAS.lan>
2026-03-31 22:08:26 +0800

    Linux bug

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-
+#include <unistd.h>
 #include "add.h"
 
 int main() {
     printf("Hello, World!\n");
     int ret = add(1, 2);
     printf("Result of 1+2 is %d", ret);
+    sleep(5);
+    scanf("%d", &ret);
     return 0;
 }
